0b042df985606d7207d34b972359ecd8d291b452
Daniel Zaltsman <daniel@Daniels-MacBook-Air.local>
2024-06-06 02:35:00 -0700

    implement redux store and data fetch in app, add components to sidebar for product info, add libraries for graph and table, implement graph and table with overlay and sorting

--- retail-sales-page/src/App.tsx
+++ retail-sales-page/src/App.tsx
@@ -2,16 +2,20 @@ import { Container, Row } from "react-bootstrap";
 import "./App.css";
 import NavBar from "./components/Navbar/Navbar";
 import RetailPage from "./pages/RetailPage/RetailPage";
+import { setProduct } from "./app/features/product/productSlice";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import { fetchProductData } from "./api/mockApi";
+import { useAppDispatch } from "./app/hooks/hooks";
 
 const App: React.FC = () => {
-   // useEffect(() => {
-   //   first
+   const dispatch = useAppDispatch();
 
-   //   return () => {
-   //     second
-   //   }
-   // }, [third])
+   useEffect(() => {
+      fetchProductData()
+         .then((data) => dispatch(setProduct(data.data)))
+         .catch((error) => console.error("Error fetching data:", error));
+   }, [dispatch]);
 
    return (
       <div id="app">
